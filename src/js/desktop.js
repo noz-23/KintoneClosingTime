df09336f1c0e4829b5f42a7e61d71faf98590f5a
noz <160399039+noz-23@users.noreply.github.com>
2024-03-30 19:27:33 +0900

    makeNow

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,5 +1,5 @@
 /*
- *締切時間
+ *組織締切時間
  * Copyright (c) 2024 noz-23
  *  https://github.com/noz-23/
  *
@@ -40,17 +40,22 @@ jQuery.noConflict();
     'app.record.create.submit', // 作成表示
     'app.record.edit.submit',   // 編集表示
     'app.record.index.submit',  // 一覧表示
-    'app.record.detail.show', // 作成表示
+    //'app.record.detail.show', // 作成表示
   ];
 
-  kintone.events.on(EVENTS, async (events_) => {
+  kintone.events.on(EVENTS, async (events_) =>{
     //console.log('events_:%o',events_);
     // 設定の読み込み
     var config =kintone.plugin.app.getConfig(PLUGIN_ID_);
     console.log('config:%o',config);
     
+    //
+    var loginUser =kintone.getLoginUser();
+    console.log('loginUser:%o',loginUser);
+
     const readDate =config[ParameterFieldDate];   // 日付フィールド名
 
+    // 日付は数値化した方が比較が楽なので数値化
     var toDay =getNumDate( new Date());
     var tmpDay =new Date();
     tmpDay.setDate(tmpDay.getDate()+1);
@@ -60,42 +65,110 @@ jQuery.noConflict();
     console.log('date:today[%o] read[%o] tommorow[%o]',toDay,readDay,tomorrow);
 
     if( readDay >=tomorrow){
+     // 未来の日付
       //console.log('date:today[%o] < read[%o]',toDay,readDay);
       return events_;
     }
-    //if( readDay <toDay){
-    //if (window.confirm('締切です')) {
+    if( readDay <toDay){
+      // 過去の日付
       // 公式のエラー表示＆キャンセル方法
-      events_.error='締切';
+      events_.error=TimeError(loginUser.language);
       return events_;
-    //}
-    //}
+    }
+    // 当日
+    var paramUsers ={code:loginUser.code};
+    console.log('paramUsers:%o',paramUsers);
+    var listOrgan =await kintone.api(kintone.api.url('/v1/user/organizations', true), 'GET',paramUsers);
+    console.log('listOrgan:%o',listOrgan);
     
     // テキストからJSONへ変換
-    var listCount =config[ParameterCountRow];
-    var listRow =JSON.stringify(config[ParameterListRow]);
+    var listCount =Number(config[ParameterCountRow]);
+    if( listCount ==0)
+    {
+      return events_;
+    }
+
+    // 設定列の変換
+    var listRow =JSON.parse(config[ParameterListRow]);
     console.log('listRow:%o',listRow);
 
-    var loginUser =kintone.getLoginUser();
-    var paramUsers ={code:loginUser.code};
-    var listOrgan =await kintone.api(kintone.api.url('/v1/organizations', true), 'GET',paramUsers);
-    console.log('listOrgan:%o',listOrgan);
+    for( var row of listRow){
+    // 各列での判定
+    var flg =false;
+      console.log('row:%o',row);
+      for(var orgn of listOrgan.organizationTitles){
+        // ユーザーの所属組織
+        console.log('orgn:%o',orgn);
 
+        // 設定にあるか検索
+        var find =row.ListChecked.find( d => d ==orgn.organization.code);
+        console.log('find:%o',find);
+
+        if( typeof find !=='undefined'){
+          flg =true;
+          break;
+        }
+      }
+      // 設定にあった場合は時間比較
+      if( flg ==true){
+      // 時間は数値化した方が比較が楽なので数値化
+        var nowTime =getNumTime(new Date());
+        var rowTime =Number(row.Time.replace(':',""));
+        console.log('nowTime[%o] rowTIme[%o]',nowTime,rowTime);
+
+        if(nowTime >rowTime){
+          // 設定時間を超えてたら保存させない
+          events_.error=TimeError(loginUser.language);
+        }
+      }
+    }
+    
+    //console.log("%o",error_stop);
     return events_;
   });
 
+  /*
+  締切時間エラーの文字
+   引数　：lang_ ユーザの言語設定
+   戻り値：エラー文字
+  */
+   const TimeError=( lang_)=>{
+    return ( lang_ =='ja') ? ('締切'):('It\'s Closing');
+  }
+
+  /*
+  日付(数値)関数取得
+   引数　：date_ 日付時間型
+   戻り値：数値 ( 2024/03/30  なら 20240330 )
+  */
   const getNumDate=(date_)=>{
-    console.log('date_:%o',date_);
+    //console.log('date_:%o',date_);
 
     var year = date_.getFullYear();
     var month = ("0" + String(date_.getMonth() + 1)).slice(-2);
     var day = ("0" + String(date_.getDate())).slice(-2);
 
     var strDate=year+month+day;
-    console.log('strDate:%o',strDate);
+    //console.log('strDate:%o',strDate);
     return Number(strDate);
   }
- 
+
+  /*
+  時間(数値)関数取得
+   引数　：time_ 日付時間型
+   戻り値：数値 ( 7:45 なら 745 )
+  */
+   const getNumTime=(time_)=>{
+    //console.log('time_:%o',time_);
+
+    var hour = ("0" + String(time_.getHours())).slice(-2);
+    var minute = ("0" + String(time_.getMinutes() )).slice(-2);
+
+    var strTime=+hour+minute;
+    //console.log('strTime:%o',strTime);
+    return Number(strTime);
+  }
+  
   /*
   スリープ関数
    引数　：ms_ ms単位のスリープ時間
